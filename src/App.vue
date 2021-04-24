63d89197a5fb478006666ed1d2f296a502632a63
mercuryZ <15210820708@163.com>
2021-04-24 16:58:04 +0800

    navigation-drawer

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,25 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer
+      fixed
+      clipped
       v-model="drawer"
-      :clipped="$vuetify.breakpoint.mdAndUp"
+      temporary
     >
-      <v-list dense>
-        <template v-for="item in items">
-          <v-layout
-            v-if="item.heading"
-            :key="item.heading"
-            row
-            align-center
-          >
-            <v-flex xs6>
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
-            </v-flex>   
-          </v-layout>
-
-        </template>
-      </v-list>
+      <v-list-item>
+        <v-list-item-content class="white">
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
     </v-navigation-drawer>
-
-    <v-app-bar
-      :clipped-left="$vuetify.breakpoint.mdAndUp"
+    <v-app-bar  
       app
+      clipped
       color="blue darken-3"
       dark
     >
@@ -34,7 +28,21 @@
         class="ml-0 pl-3"
       >
         <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <router-link tag="span" class="hidden-sm-and-down" to="/">Google Contacts</router-link>
+        <router-link tag="span" class="hidden-sm-and-down" to="/">THE Lab </router-link>
+        <v-btn
+          icon
+          large
+        >
+          <v-avatar
+            size="32px"
+            item
+          >
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+              alt="Vuetify"
+            >
+            </v-img></v-avatar>
+        </v-btn>
       </v-toolbar-title>
       <v-text-field
         flat
@@ -51,21 +59,8 @@
       <v-btn icon>
         <v-icon>mdi-home</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        large
-      >
-        <v-avatar
-          size="32px"
-          item
-        >
-          <v-img
-            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
-            alt="Vuetify"
-          >
-          </v-img></v-avatar>
-      </v-btn>
     </v-app-bar>
+
   </v-app>
 </template>
 
@@ -78,7 +73,7 @@
         ['mdi-send', '关于', '/about'],
       ],
       items: [
-      { icon: 'contacts', text: 'Contacts', to: '/contacts' },
+      { icon: 'contacts', text: 'Contacts', to: '/contacts', heading: '2018' },
       { icon: 'history', text: 'Frequently contacted', to: '/page1' },
       { icon: 'content_copy', text: 'Duplicates', to: '/404' },
       {
